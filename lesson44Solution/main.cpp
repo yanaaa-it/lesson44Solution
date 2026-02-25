ca6d3a37d0e59c9402ec79517c17e7818558207f
yanaaa-it <yaninalikemiki@gmail.com>
2026-02-25 18:48:23 +0300

    new

--- lesson44Solution/main.cpp
+++ lesson44Solution/main.cpp
@@ -1,7 +1,36 @@
-#include"util.h"
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+bool search_binary(int array[10], int length,int value) {
+	int first = 0;
+	int last = length - 1;
+	while (first<=last) {
+		int mid = (first + last) / 2;
+		if (array[mid]==value){
+			return true;
+		}
+		else if(array[mid] > value){
+			first = mid + 1;
+		}
+		else {
+			last = mid + 1;
+		}
+	}
+	return false;
+}
 
 int main() {
+	int array[]{1,2,3,4,5,6,7,8,9,10};
+	int length{10};
 
+	int value = 7;
+
+	cout << "does value exist? - ";
+	cout << (search_binary(array, length,value) ? "yes" : "no")<<endl;
 
 	return 0;
 }
+
